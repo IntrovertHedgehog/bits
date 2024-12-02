c5606a535262a645bfaf2dacb60937209a22aa8c
IntrovertHedgehog <larby2409@gmail.com>
2024-12-02 15:52:28 +0800

    update template

--- templates.cc
+++ templates.cc
@@ -1,13 +1,24 @@
+/*
+ * Copyright 2024 Le Van Minh
+ */
+
 #include <bits/stdc++.h>
 #include <fmt/base.h>
 #include <fmt/ranges.h>
 
-using namespace std;
+#include <functional>
+#include <ios>
+#include <iostream>
+#include <string>
 
 // ordered_set
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
-using namespace __gnu_pbds;
+using __gnu_pbds::null_type;
+using __gnu_pbds::rb_tree_tag;
+using __gnu_pbds::tree;
+using __gnu_pbds::tree_order_statistics_node_update;
+using std::less;
 typedef tree<int, null_type, less<int>, rb_tree_tag,
              tree_order_statistics_node_update>
     ordered_set;
@@ -15,6 +26,8 @@ typedef tree<int, null_type, less<int>, rb_tree_tag,
 // pretty print containers
 #include <sstream>
 
+using std::string;
+using std::stringstream;
 template <typename T>
 string dumpContainer(const T& container, std::string sep) {
   typename T::const_iterator itr;
@@ -33,6 +46,8 @@ string dumpContainer(const T& container, std::string sep) {
 }
 
 // main with fast io
+using std::cin;
+using std::ios_base;
 int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
